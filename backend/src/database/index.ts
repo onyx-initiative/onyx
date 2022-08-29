4ed03900a9d30aee4ab83722eca2a63a589d29ef
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2022-08-28 20:00:56 -0400

    altered getDB

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -3,7 +3,8 @@ const { Client } = require("pg");
 const client = new Client(process.env.DATABASE_URL);
 
 // Include client.end() when used
-const getDB = async () => {
-    await client.connect();
+const getDB = () => {
+    // Add: await client.connect();
+    // and client.end() when used
     return client;
 }
