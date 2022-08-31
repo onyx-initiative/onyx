6d009da10ed127be949643c1a496f2c0bffce15e
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2022-08-30 20:29:28 -0400

    added initial database schema

---
+++ backend/src/types/db.types.ts
@@ -0,0 +1,77 @@
+type View = {
+    id: number;
+    viewName: string;
+    filters: Filter[];
+}
+
+type Filter = {
+    id: number;
+    industryFilter?: "finance" | "consulting" | "technology" | "design" | "other"; // Others?
+    ProvinceFilter?: "AB" | "BC" | "MB" | "NB" | "NL" | "NS" | "NT" | "NU" | "ON" | "PE" | "QC" | "SK" | "YT";
+    CityFilter?: string; // This will be based on current cities in the database
+    yearFilter?: "freshman" | "sophomore" | "junior" | "senior" | "other";
+    durationFilter?: "4 months" | "6 months" | "8 months" | "12 months" | "part-time" | "full-time" | "other";
+}
+
+type Scholar = {
+    id: number;
+    name: string;
+    email: string;
+    jobApplications: string;
+    workHistory?: string[];
+    status: "current" | "alumni" | "new graduate" | "intern"; 
+    views: View[]; // The specific sets of filters the specific scholar has saved
+    profilePicture?: string;
+    year: string;
+    school: string;
+    major: string;
+    city: string;
+    province: string;
+    registrationDate: string; // Used to archive them following 2 year alum period
+    skills: string[]; // Specific skills/interests the scholar has
+    notifications: boolean; // True if enabled, false if disabled
+}
+
+type Admin = {
+    id: number;
+    name: string;
+    email: string;
+}
+
+type Job = {
+    jobTitle: string;
+    jobDescription: string;
+    company: string; // This would be a relation in the database and point to a company object
+    city: string;
+    province: string;
+    jobSource: string; // where the job came from (ie. initially website and later (p1/p2) linkedin, etc.)
+    totalViews: number; // total number of views the job has received (p1/p2)
+    totalApplications: number; // total number of applications the job has received
+    jobStatus: "open" | "closed"; // open if the job is still accepting applications, closed if not
+    jobType: "full time" | "part time" | "contract" | "internship"; 
+    jobCategory: "software" | "hardware" | "design" | "marketing" | "business" | "other"; // Can add more as they come up
+    jobSkills: string[]; // Specific skills/interests the job has
+    salaryRange: string; // salary range (ie. $10,000 - $15,000) - prorated based on job lengther
+    jobLength: number; // job length in number of months
+    postDate: string; 
+    applicationDeadline: string;
+    description: string; 
+    contactEmail: string;
+    feature: boolean; // True if featured on the landing page, false if not
+    additionalInstructions?: string; 
+    howToApply?: "email" | "link" | "form"; // Others can be added as they come up
+}
+
+type Employer = {
+    id: number;
+    name: string;
+    logo: string;
+    city: string;
+    province: string;
+    websiteUrl: string;
+    description: string;
+    availableJobs: Job[];
+
+    // P1+
+    videos?: string[]; // Videos that showcase the company
+}
---
+++ meetings/specifications.md
@@ -0,0 +1,46 @@
+## Meeting August 30, 2022
+
+- Search and browse jobs
+    - Filter by industry and company, year, duration, season, position
+    - Jobs are manually added
+    - Get and excel file of links and then post them
+- 2 personas
+    - Admin 
+        - Add, update and delete records
+        - Partner profiles (later)
+        - Can immediately click on and get linked to a new job
+        - (later) if theres an event and tag employers
+    - Scholar
+        - Only view, no ability to update any info
+        - Click on a link that takes them to the partner website (later)
+        - Know if a link gets clicked
+        - View
+            - Search and create a view that works for them
+                - ie. A mtl student can see jobs in mtl, persist on a per scholar basis by saving views
+                - Notifications (later)
+        - Applied page to show what a scholar has applied to
+            - Also get a link to 
+            - show ones they've applied to and ones they're watching
+        - Add alum (only alum for next 2 years)
+            - Intern, new grad and alum
+        - They'll create their own dashboard and be sent updates based on that
+    - Job
+        - id
+    - (later) Employer page
+- Login 
+    - Need to find a vendor for SAML/SSO
+- Notifications
+    - Change to your view
+        - Focus on the specific person, ie. a job was added that fits your filters
+        - Fits the specific views that they created & can have multiple views
+            - Only notified when one thing changes that matches one of their views
+    - Expiration
+        - Notification to both the scholars and the admin
+        - If a job expires it should be removed from the view and archived
+- Will want to add videos to the employer profiles later
+- Want a landing page with featured employers a la jobboard
+    - Latest posts and greatest number of opportunities
+- AWS lambda
+- 150/cohort
+- Add the upcoming events on the welcome page
+- Add link to linkedin learning
