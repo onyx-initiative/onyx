8dd6105f35e5ccfdb98b5f1d869dbf7884272127
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2022-09-18 15:52:51 -0400

    added sample scholar data for wrtiting tests

---
+++ backend/src/integration/mock-data/scholarData.ts
@@ -0,0 +1,28 @@
+const createScholarData = {
+    1: {
+        "name": "Michael Dawes",
+        "email": "michaelanthony.dawes@mail.utoronto.ca",
+        "current": true,
+        "gradYear": "2024",
+        "school": "University of Toronto",
+        "major": "Business",
+        "city": "Toronto",
+        "province": "Ontario",
+        "registrationDate": "2022-09-18",
+        "notifications": true,
+        "skills": ["PowerPoint", "Excel", "Python"]
+    }, 
+    2: {
+        "name": "Cole Purboo",
+        "email": "cpurbs@icloud.com",
+        "current": true,
+        "gradYear": "2024",
+        "school": "University of Toronto",
+        "major": "Business",
+        "city": "Toronto",
+        "province": "Ontario",
+        "registrationDate": "2022-09-18",
+        "notifications": true,
+        "skills": ["PowerPoint", "Excel", "Python"]
+      }
+}
