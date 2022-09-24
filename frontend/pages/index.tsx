9f39930cdc3221ba590a6096f6c8406f122465b4
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2022-09-24 11:34:49 -0400

    added initial structure and examples

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,11 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Scholar from './Scholar'
 
 const Home: NextPage = () => {
   return (
     <div>
-      Hello
+      <Scholar />
     </div>
   )
 }
---
+++ frontend/src/components/general/SearchBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function SearchBar(props: any) {
+  return (
+    <div className=''>
+        SearchBar
+    </div>
+  )
+}
+
+
