51e0fb4dbe31e6f3501d451742c4df3d3060c2f9
ColePurboo <cole.purboo@icloud.com>
2022-11-29 13:54:53 -0500

    Initial EmployerInfo commit

--- frontend/pages/Employers.tsx
+++ frontend/pages/Employers.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Navbar from '../src/components/general/Navbar'
 
 export default function Employers() {
   return (
-    <div>Employers</div>
+    <div>Employers
+      <Navbar/>
+      
+    </div>
   )
 }
---
+++ frontend/src/components/employer/EmployerInfo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function EmployerInfo(props: any) {
+
+    return (
+        <div className="">
+            <Image 
+            src={logo}
+            width={100}
+            height={50}/>
+        </div>
+
+
+    )
+}
