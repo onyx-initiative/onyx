81941f359d0fc429e952cad8ae1141878594e79b
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2023-01-20 15:19:30 -0500

    added admin queries and mutations to frontend

--- frontend/pages/ServerCheck.tsx
+++ frontend/pages/ServerCheck.tsx
@@ -1,18 +1,43 @@
 import React from 'react'
-import { useQuery } from '@apollo/client'
+import { useQuery, useMutation, useLazyQuery } from '@apollo/client'
 import { GET_ADMINS } from '../graphql/queries/adminQueries'
+import { CREATE_ADMIN } from '../graphql/mutations/adminMutations'
 
 
 // This is only for testing, delete after
 
 export default function ServerCheck() {
 
-    const {loading, error, data } = useQuery(GET_ADMINS)
+    const {
+        loading: adminLoading, 
+        error: adminError, 
+        data: adminData, 
+        refetch: refetchAdmins 
+    } = useQuery(GET_ADMINS, {
+        pollInterval: 500,
+        fetchPolicy: 'cache-and-network'
+    })
+    const [getAdmins, 
+        { 
+            loading: lazyAdminLoading, 
+            error: lazyAdminError, 
+            data: lazyAdminData 
+        }
+    ] = useLazyQuery(GET_ADMINS, {
+        // This ensures that the query is up to date
+        fetchPolicy: 'cache-and-network'
+    })
+    const [createAdmin, { data, loading, error }] = useMutation(CREATE_ADMIN, {
+        // Every time this mutation is called, the query will be refetched
+        // This is a better solution than the one above
+        refetchQueries: [{ query: GET_ADMINS }]
+    });
 
     return (
         <div>
             <button
                 onClick={() => {
+                    createAdmin({ variables: { name: "Test", email: "new_email@onyx.org"}})
                     console.log(data)
                 }}
             >
@@ -20,8 +45,8 @@ export default function ServerCheck() {
             </button>
             <button
                 onClick={() => {
-                    GET_ADMINS
-                    console.log(data)
+                    getAdmins()
+                    console.log(lazyAdminData)
                 }}
             >
                 Check Admins
