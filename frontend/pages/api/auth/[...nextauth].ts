05b3b9411118d831c6cd3725c14994e6f9e60852
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2023-01-21 11:39:07 -0500

    update next auth

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -12,29 +12,29 @@ export default NextAuth({
         })
     ],
     secret: process.env.NEXT_PUBLIC_JWT_SECRET as string,
-    // pages: {
-    //     signIn: "/Login",
-    //     signOut: "/",
-    //     error: "/Login",
-    //     verifyRequest: "/Login",
-    // },
-    // session: {
-    //     // Choose how you want to save the user session.
-    //     // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
-    //     // If you use an `adapter` however, we default it to `"database"` instead.
-    //     // You can still force a JWT session by explicitly defining `"jwt"`.
-    //     // When using `"database"`, the session cookie will only contain a `sessionToken` value,
-    //     // which is used to look up the session in the database.
-    //     strategy: "database",
+    pages: {
+        signIn: "/Login",
+        signOut: "/",
+        error: "/Login",
+        verifyRequest: "/Login",
+    },
+    session: {
+        // Choose how you want to save the user session.
+        // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
+        // If you use an `adapter` however, we default it to `"database"` instead.
+        // You can still force a JWT session by explicitly defining `"jwt"`.
+        // When using `"database"`, the session cookie will only contain a `sessionToken` value,
+        // which is used to look up the session in the database.
+        strategy: "database",
 
-    //     // Seconds - How long until an idle session expires and is no longer valid.
-    //     // maxAge: 30 * 24 * 60 * 60, // 30 days
-    //     maxAge: 60 * 60 * 24, // 1 day
+        // Seconds - How long until an idle session expires and is no longer valid.
+        // maxAge: 30 * 24 * 60 * 60, // 30 days
+        maxAge: 60 * 60 * 24, // 1 day
 
-    //     updateAge: 24 * 60 * 60, // 24 hours
+        updateAge: 24 * 60 * 60, // 24 hours
         
-    //     generateSessionToken: () => {
-    //         return randomUUID?.() ?? randomBytes(32).toString("hex")
-    //     }
-    // },
+        generateSessionToken: () => {
+            return randomUUID?.() ?? randomBytes(32).toString("hex")
+        }
+    },
 });
