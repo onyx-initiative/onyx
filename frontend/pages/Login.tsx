27bc0a7d8873415305e1457bb57f740b174fe342
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2023-01-21 12:32:02 -0500

    edits

--- frontend/pages/Login.tsx
+++ frontend/pages/Login.tsx
@@ -24,7 +24,7 @@ export default function Login() {
                     <div className={styles.scholarLogin}>
                         <FaGoogle size={28} />
                         <button
-                            onClick={() => signIn()}
+                            onClick={() => signIn('google')}
                         >
                             Login with Google
                         </button>
