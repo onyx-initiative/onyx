4d22fcab6a90e96139b4a6f835db44ada6bb54e5
ColePurboo <cole.purboo@icloud.com>
2023-01-27 10:42:30 -0500

    deleted mock data

--- frontend/src/components/employer/EmployerBlock.tsx
+++ frontend/src/components/employer/EmployerBlock.tsx
@@ -18,7 +18,7 @@ type EmployerBlock = {
 export default function EmployerBlock({logo, companyName, link, info}: EmployerBlock) {
     const [opened, setOpened] = useState(false);
     
-    const jobs = sampleJob
+  
 
     return (
         
@@ -60,45 +60,3 @@ export default function EmployerBlock({logo, companyName, link, info}: EmployerB
 
 
 
-//Sample Data
-const sampleJob: Job[] = [
-    {
-    job_id: '123',
-    employer_id: '123',
-    admin_id: '123',
-    title: 'Software Engineer',
-    description: "This is a sample job description for a software engineer in Toronto. Please visit www.mckinsey.com/careers to apply.",
-    job_type: 'Full Time',
-    location: 'Toronto, ON',
-    applicant_year: [2023, 2024],
-    deadline: new Date(),
-    total_views: 0,
-    tags: ['Software', 'Engineering', 'Internship', 'DevOps', 'Backend'],
-    },
-    {
-      job_id: '456',
-      employer_id: '456',
-      admin_id: '456',
-      title: 'Business Analyst',
-      description: "This is a sample job description for a software engineer in Toronto. Please visit www.mckinsey.com/careers to apply.",
-      job_type: 'Full Time',
-      location: 'Toronto, ON',
-      applicant_year: [2023, 2024],
-      deadline: new Date(),
-      total_views: 0,
-      tags: ['Software', 'Engineering', 'Internship', 'DevOps', 'Backend'],
-    },
-    {
-      job_id: '2',
-      employer_id: '2',
-      admin_id: '2',
-      title: 'Software Engineer',
-      description: "This is a sample job description for a software engineer in Toronto. Please visit www.mckinsey.com/careers to apply.",
-      job_type: 'Full Time',
-      location: 'Toronto, ON',
-      applicant_year: [2023, 2024],
-      deadline: new Date(),
-      total_views: 0,
-      tags: ['Software', 'Engineering', 'Internship', 'DevOps', 'Backend'],
-    },
-]
