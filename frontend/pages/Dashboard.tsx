3ecbab364fdc946eb896fb7a1134968b5013d9f8
Rayce Ramsay <ramsayrayce@gmail.com>
2024-08-27 14:38:13 -0400

    Style dashboard charts

--- frontend/pages/Dashboard.tsx
+++ frontend/pages/Dashboard.tsx
@@ -1,44 +1,94 @@
-import { useQuery } from "@apollo/client";
-import {
-  GET_ANALYTICS_DASHBOARD_DATA
-} from "../graphql/queries/analyticsQueries";
-import { Chart } from "react-google-charts";
-import styles from "../styles/Dashboard.module.css";
-import Navbar from "../src/components/general/Navbar";
+import { useQuery } from '@apollo/client'
+import { GET_ANALYTICS_DASHBOARD_DATA } from '../graphql/queries/analyticsQueries'
+import { Chart } from 'react-google-charts'
+import styles from '../styles/Dashboard.module.css'
+import Navbar from '../src/components/general/Navbar'
 
 function Dashboard() {
   const { data, loading, error } = useQuery(GET_ANALYTICS_DASHBOARD_DATA, {
-    variables: { startDate: "2024-01-01", endDate: "2024-08-01" },
-  });
+    variables: { startDate: '2024-01-01', endDate: '2024-08-01' },
+  })
 
-  if (loading) return <div>loading</div>;
-  if (error) return <div>error</div>;
+  if (loading) return <div>loading</div>
+  if (error) return <div>error</div>
 
-
-  const dailyClickCounts = [
-    ["Date", "Daily Count"],
-    ...data.jobClicksDaily.map((item: any) => [item.date, item.count]),
-    ...data.applyClicksDaily.map((item: any) => [item.date, item.count]),
-    ...data.employerClicksDaily.map((item: any) => [item.date, item.count])
-  ];
-  const dailyChartOptions = {
-    title: "Daily Clicks",
-  };
+  const types = ['Job', 'Apply', 'Employer']
+  const intervals = ['Daily', 'Weekly', 'Monthly', 'Yearly']
+  let clickChartsIntervals = []
+  for (const type of types) {
+    for (const interval of intervals) {
+      const dataKey = `${type.toLowerCase()}Clicks${interval}`
+      const clickCounts = [
+        ['Date', `${interval} ${type} Count`],
+        ...data[dataKey].map((item: any) => [item.date, item.count]),
+      ]
+      const chartOptions = {
+        title: `${interval} ${type} Clicks`,
+        titlePosition: 'none',
+        legend: 'none',
+        backgroundColor: 'transparent',
+        chartArea: {
+          width: '100%',
+          height: '100%',
+          bottom: '30',
+        },
+        vAxis: {
+          gridlines: {
+            color: '#333',
+          },
+          minorGridlines: {
+            count: 0,
+          },
+          textPosition: 'in',
+          textStyle: {
+            color: '#999',
+          },
+          slantedText: false,
+          maxAlternation: 1,
+        },
+        hAxis: {
+          textPosition: 'out',
+          textStyle: {
+            color: '#999',
+          },
+          slantedText: false,
+          maxAlternation: 1,
+        },
+        crosshair: {
+          trigger: 'focus',
+          color: 'white',
+          orientation: 'vertical',
+        },
+        tooltip: {
+          isHtml: true,
+        },
+        axisTitlesPosition: 'none',
+      }
+      clickChartsIntervals.push({ clickCounts, chartOptions })
+    }
+  }
 
   return (
     <>
       <Navbar />
-      <main>
-        <Chart
-          chartType="Line"
-          width="80%"
-          height="400px"
-          data={dailyClickCounts}
-          options={dailyChartOptions}
-        />
+      <main className={styles.wrapper}>
+        <div className={styles.charts}>
+          {clickChartsIntervals.map((item: any) => (
+            <div key={item.chartOptions.title} className={styles.chartContainer}>
+              <Chart
+                chartType='AreaChart'
+                width='100%'
+                height='100%'
+                data={item.clickCounts}
+                options={item.chartOptions}
+                className={styles.chart}
+              />
+            </div>
+          ))}
+        </div>
       </main>
     </>
-  );
+  )
 }
 
-export default Dashboard;
+export default Dashboard
