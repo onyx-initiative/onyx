55a0733f54beb88e9a1519986e6ea17d80d5820b
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2026-03-02 20:37:10 -0500

    Update frontend refresh for sending jobs

--- frontend/src/components/admin/ExcelReader.tsx
+++ frontend/src/components/admin/ExcelReader.tsx
@@ -1,7 +1,7 @@
-import React, { useState, ChangeEvent, useEffect } from 'react';
+import React, { useState, ChangeEvent, useEffect } from "react";
 import * as XLSX from "xlsx";
-import { BATCH_CREATE_JOBS } from '../../../graphql/mutations/jobMutations';
-import { useMutation } from '@apollo/client';
+import { BATCH_CREATE_JOBS } from "../../../graphql/mutations/jobMutations";
+import { useMutation } from "@apollo/client";
 import styles from "../../../styles/components/AdminPageButtons.module.css";
 
 interface ExcelData {
@@ -10,17 +10,11 @@ interface ExcelData {
 
 const ExcelReader: React.FC = () => {
   const [data, setData] = useState<ExcelData[]>([]);
-  const [fileName, setFileName] = useState<string>('');
-  const [batch_create_jobs] = useMutation(BATCH_CREATE_JOBS, {
-    variables: {
-      // @todo: Update with the current admin id
-      adminId: "1",
-      jobs: formatData(data)
-    }
-  })
+  const [fileName, setFileName] = useState<string>("");
+  const [batch_create_jobs] = useMutation(BATCH_CREATE_JOBS);
 
   console.log(data);
-  
+
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
@@ -30,12 +24,12 @@ const ExcelReader: React.FC = () => {
         const fileContent = evt.target?.result as string;
 
         // Check if the file is a CSV file
-        const isCSV = file.name.endsWith('.csv');
+        const isCSV = file.name.endsWith(".csv");
 
         // Parse the file using SheetJS
         const workbook = XLSX.read(fileContent, {
-          type: isCSV ? 'string' : 'binary',
-          cellDates: true
+          type: isCSV ? "string" : "binary",
+          cellDates: true,
         });
 
         // Get the first worksheet
@@ -48,7 +42,7 @@ const ExcelReader: React.FC = () => {
       };
 
       // Read the file based on its type
-      if (file.name.endsWith('.csv')) {
+      if (file.name.endsWith(".csv")) {
         reader.readAsText(file);
       } else {
         reader.readAsBinaryString(file);
@@ -63,22 +57,37 @@ const ExcelReader: React.FC = () => {
         Add jobs from an Excel or CSV file
       </label>
       <div className={styles.upload}>
-        <input id="file-upload" type="file" onChange={handleFileChange} className={styles.uploadButton} />
+        <input
+          id="file-upload"
+          type="file"
+          onChange={handleFileChange}
+          className={styles.uploadButton}
+        />
         {fileName && <p>Selected file: {fileName}</p>}
       </div>
       {/* Show the contents from the csv/excel */}
       {/* <pre>{JSON.stringify(formatData(data), null, 2)}</pre> */}
-      <button className={styles.submitButton} onClick={() => {
-        batch_create_jobs()
-        .then((res) => {
-          if (res.data?.batchCreateJobs) {
-            alert('Jobs created successfully');
-          }
-        }).catch((err) => {
-          alert('Error creating jobs. Please make sure all employer names are exactly as they appear in the database and check the fields.');
-          console.log(`Error creating jobs: ${err}`);
-        })
-      }}
+      <button
+        className={styles.submitButton}
+        onClick={() => {
+          batch_create_jobs({
+            variables: {
+              adminId: "1",
+              jobs: formatData(data),
+            },
+          })
+            .then((res) => {
+              if (res.data?.batchCreateJobs) {
+                alert("Jobs created successfully");
+              }
+            })
+            .catch((err) => {
+              alert(
+                "Error creating jobs. Please make sure all employer names are exactly as they appear in the database and check the fields.",
+              );
+              console.log(`Error creating jobs: ${err}`);
+            });
+        }}
       >
         Create Jobs
       </button>
@@ -92,9 +101,17 @@ export default ExcelReader;
 const formatData = (data: any) => {
   // Convert applicant year and tags to arrays
   data.map((applicant: any) => {
-    applicant.applicant_year = applicant.applicant_year ? applicant.applicant_year.toString().split(',').map((year: string) => parseInt(year)) : null;
-    typeof applicant.tags === 'string' ? applicant.tags = applicant.tags.split(',').map((tag: string) => tag.trim()) : applicant.tags = applicant.tags;
-    
+    applicant.applicant_year = applicant.applicant_year
+      ? applicant.applicant_year
+          .toString()
+          .split(",")
+          .map((year: string) => parseInt(year))
+      : null;
+    typeof applicant.tags === "string"
+      ? (applicant.tags = applicant.tags
+          .split(",")
+          .map((tag: string) => tag.trim()))
+      : (applicant.tags = applicant.tags);
   });
   return data;
-}
+};
